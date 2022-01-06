cf2dff006892d3e7e5bc3b71d10735d53e318f2f
SST6040 <61400018+SST6040@users.noreply.github.com>
2022-01-06 21:15:48 +0800

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA3pwKPP-O22Qm7bYsR9WL51oi_q6g2f_0",
+    authDomain: "fyp1-8b053.firebaseapp.com",
+    projectId: "fyp1-8b053",
+    storageBucket: "fyp1-8b053.appspot.com",
+    messagingSenderId: "446139133563",
+    appId: "1:446139133563:web:ffe8077c09d528bbf4bcde",
+    measurementId: "G-56FGM58VYS"
+    }
+};
---
+++ environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+  apiKey: "AIzaSyA3pwKPP-O22Qm7bYsR9WL51oi_q6g2f_0",
+  authDomain: "fyp1-8b053.firebaseapp.com",
+  projectId: "fyp1-8b053",
+  storageBucket: "fyp1-8b053.appspot.com",
+  messagingSenderId: "446139133563",
+  appId: "1:446139133563:web:ffe8077c09d528bbf4bcde",
+  measurementId: "G-56FGM58VYS"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
